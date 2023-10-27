037934d8de1a95e6a8173661a229939aec3ab83e
Admin <pgabor1021@gmail.com>
2023-10-27 13:39:39 +0200

    5

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -6,9 +6,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class DemoController {
 
-    @GetMapping("/index")
-    public String test() {
-        return "test";
+    @GetMapping("/")
+    public String index() {
+        return "index";
     }
-
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
 }
